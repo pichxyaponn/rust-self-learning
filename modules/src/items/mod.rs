d4cb896f80a42e783b786616bf89308ae75cafbf
pichxyaponn <pichxyaponn@gmail.com>
2025-04-26 01:28:07 +0700

    learned: modules

---
+++ modules/src/items/mod.rs
@@ -0,0 +1,15 @@
+pub mod potions {
+    pub fn use_item() {
+        println!("Used a potion");
+    }
+}
+pub mod weapons {
+    pub fn use_item() {
+        println!("Used a weapon");
+    }
+}
+pub mod maps {
+    pub fn use_item() {
+        println!("Used a map");
+    }
+}
---
+++ modules/src/main.rs
@@ -0,0 +1,7 @@
+use modules::items::*;
+
+fn main() {
+    potions::use_item();
+    weapons::use_item();
+    maps::use_item();
+}
